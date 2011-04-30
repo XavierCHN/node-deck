77602734ea3aac5ca234ccedfe2eba6726eb6f41
James Halliday <mail@substack.net>
2011-04-30 16:26:51 -0700

    more abstract shuffler test

--- test/shuffle.js
+++ test/shuffle.js
@@ -1,24 +1,23 @@
 var deck = require('deck');
 var assert = require('assert');
 var eql = assert.deepEqual;
-var ok = assert.ok;
 
-exports.shuffle = function () {
+function shuffler (fn, x) {
     var xs = [0,1,2,3,4,5,6,7,8,9];
-    var xs_ = deck.shuffle(xs);
+    var xs_ = fn(xs);
     eql(xs, [0,1,2,3,4,5,6,7,8,9], 'shuffle mutated its argument');
     eql(xs.length, 10);
-    ok(xs.every(function (x) {
+    assert.ok(xs.every(function (x) {
         return xs_.indexOf(x) >= 0
     }));
 };
 
-exports.shuffleObj = function () {
-    var xs = [0,1,2,3,4,5,6,7,8,9];
-    var xs_ = deck(xs).shuffle();
-    eql(xs, [0,1,2,3,4,5,6,7,8,9], 'shuffle mutated its argument');
-    eql(xs.length, 10);
-    ok(xs.every(function (x) {
-        return xs_.indexOf(x) >= 0
-    }));
+exports.shuffle = function (xs) {
+    shuffler(deck.shuffle, xs);
+};
+
+exports.shuffleObj = function (xs) {
+    shuffler(function (ys) {
+        return deck(ys).shuffle()
+    }, xs);
 };
