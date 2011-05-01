afcee0f5b6de019f4f23f09f7a942161216700e8
James Halliday <mail@substack.net>
2011-04-30 18:32:49 -0700

    shuffle test was also being super silly about xs

--- test/normalize.js
+++ test/normalize.js
@@ -2,7 +2,7 @@ var deck = require('deck');
 var assert = require('assert');
 var eql = assert.deepEqual;
 
-exports.normalize = function (xs) {
+exports.normalize = function () {
     eql(
         deck.normalize({ a : 1, b : 3, c : 4 }),
         { a : 0.125, b : 0.375, c : 0.5 }
--- test/shuffle.js
+++ test/shuffle.js
@@ -2,7 +2,7 @@ var deck = require('deck');
 var assert = require('assert');
 var eql = assert.deepEqual;
 
-function shuffler (fn, x) {
+function shuffler (fn) {
     var xs = [0,1,2,3,4,5,6,7,8,9];
     var xs_ = fn(xs);
     eql(xs, [0,1,2,3,4,5,6,7,8,9], 'shuffle mutated its argument');
@@ -12,12 +12,12 @@ function shuffler (fn, x) {
     }));
 }
 
-exports.shuffle = function (xs) {
-    shuffler(deck.shuffle, xs);
+exports.shuffle = function () {
+    shuffler(deck.shuffle);
 };
 
-exports.shuffleObj = function (xs) {
-    shuffler(function (ys) {
-        return deck(ys).shuffle()
-    }, xs);
+exports.shuffleObj = function () {
+    shuffler(function (xs) {
+        return deck(xs).shuffle()
+    });
 };
