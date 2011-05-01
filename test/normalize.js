2b6a92203a43efdab10711b8d6115a4b8deb1d80
James Halliday <mail@substack.net>
2011-04-30 18:17:35 -0700

    also check normalize for a total < 1

--- test/normalize.js
+++ test/normalize.js
@@ -7,4 +7,10 @@ exports.normalize = function (xs) {
         deck.normalize({ a : 1, b : 3, c : 4 }),
         { a : 0.125, b : 0.375, c : 0.5 }
     );
+    
+    var t = 0.1 + 0.2 + 0.05;
+    eql(
+        deck.normalize({ a : 0.1, b : 0.2, c : 0.05 }),
+        { a : 0.1 / t, b : 0.2 / t, c : 0.05 / t }
+    );
 };
